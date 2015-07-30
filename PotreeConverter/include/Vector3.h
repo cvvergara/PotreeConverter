82408e0a792fc7628fe3c41dcd448c5b31cde8fd
mapas de vicky <vicky_vergara@hotmail.com>
2015-07-30 12:59:26 -0500

    Vector3 using boost geometry

--- PotreeConverter/include/Vector3.h
+++ PotreeConverter/include/Vector3.h
@@ -6,11 +6,12 @@
 #include <iostream>
 
 
-#ifdef BOOSTGEOM
+/*!
+   lets try boost geometry
+*/
 #include <boost/geometry.hpp>
 
 namespace bg = boost::geometry;
-#endif
 
 using std::ostream;
 #ifndef _MSC_VER
@@ -19,141 +20,115 @@ using std::max;
 
 template<class T>
 class Vector3{
-
-#if 0   // memebers are private
- private:
-	T m_x, m_y, m_z;  //  implies lots of changes
-#endif
 public:
-#ifdef BOOSTGEOM
-        bg::model::point<T, 3, boost::geometry::cs::cartesian> point3D;
-#else
-	T x, y, z;  //  implies lots of changes
-#endif
+        typedef bg::model::point<T, 3, bg::cs::cartesian> point3D;
+        point3D m_p;
 
-#if 0  // when memebers are private
-        // accessors
-        const T& x() const { return m_x; }
-        const T& y() const { return m_y; }
-        const T& z() const { return m_z; }
-         
-        T& x() { return x; }
-        T& y() { return y; }
-        T& z() { return z; }
-#endif   
-	// static long long count;
+public:
+        // TODO once all foo.x like code are changed to foo.X() then this line can go away
+	T x, y, z;
+
+        T X() const {return bg::get<0>(m_p);}
+        T Y() const {return bg::get<0>(m_p);}
+        T Z() const {return bg::get<0>(m_p);}
+
+private:
+        void set_values(const T &p_x, const T &p_y, const T &p_z) {
+        	bg::assign_values(m_p, p_x, p_y, p_z);
+        	set_values();  // TODO remove when all code changed to use foo.X()
+	}  
+        // TODO once all foo.x like code are changed to foo.X() then this function can go away
+        void set_values() {
+		x = bg::get<0>(m_p);
+		y = bg::get<1>(m_p);
+		z = bg::get<2>(m_p);
+	}
+
+public:
 
         /*!
           default constructor
         */
-#if 1
-	Vector3() = default;
-#else
-	Vector3(){
-	//	count++;
+	Vector3() {
+        	set_values(0, 0, 0);
+        }
 
-		x = 0;
-		y = 0;
-		z = 0;
-	}
-#endif
-
-        /*!
-          In c++11 to memeberwise initialize you can do this:
-
-          Vector3<double> coordinate1 = {2,3,4};
-
-          Vector3<double> coordinate2{2,3,4};
+        void set_values(const point3D &p) {
+          m_p = p;
+          set_values();  // TODO remove when all cade changed to use foo.X()
+        }
           
-          so maybe this constructor can be removed...
-          but need to change a lot of code. 
+        /*!
+          user defined constructors
         */
-	Vector3(T x, T y, T z){
-	//	count++;
-
-		this->x = x;
-		this->y = y;
-		this->z = z;
+	Vector3(const T &p_x,const T &p_y,const T &p_z) {
+		set_values(p_x, p_y, p_z);
 	}
 
-        /*!
-          user defined constructor
-        */
-	Vector3(T value){
-	//	count++;
-
-		this->x = value;
-		this->y = value;
-		this->z = value;
+	Vector3(T value) {
+		set_values(value, value, value);
 	}
 
         /*!
           copy constructor
         */
-#if 1
 	Vector3(const Vector3<T> &other) = default;
-#else
-	Vector3(const Vector3<T> &other)
-		:x(other.x), y(other.y), z(other.z)
-	{
-	//	count++;
-	}
-#endif
 
         /*!
           destructor
         */
-#if 1
 	~Vector3() = default;
-#else
-	~Vector3(){
-	//	count--;
-	}
-#endif
 
 
-#ifndef BOOSTGEOM
-	T length(){
-		return sqrt(x*x + y*y + z*z);
+	T length() const {
+		point3D origin;
+		bg::assign_values(origin, 0, 0, 0);
+                return bg::distance(m_p, origin);
+		// return sqrt(x*x + y*y + z*z);
 	}
 
-	T squaredLength(){
-		return x*x + y*y + z*z;
+	T squaredLength() const {
+		point3D origin;
+		bg::assign_values(origin, 0, 0, 0);
+                return bg::comparable_distance(m_p, origin);
+		// return x*x + y*y + z*z;
 	}
 
-	T distanceTo(Vector3<T> p){
-		return ((*this) - p).length();
+	T distanceTo(const Vector3<T> &p) const {
+                return bg::distance(m_p, p.m_p);
+		// return ((*this) - p).length();
 	}
 
-	T squaredDistanceTo(const Vector3<T> &p){
-		return ((*this) - p).squaredLength();
+	T squaredDistanceTo(const Vector3<T> &p) const{
+                return bg::comparable_distance(m_p, p.m_p);
+		// return ((*this) - p).squaredLength();
 	}
 
-	T maxValue(){
-		return max(x, max(y,z));
+	T maxValue() const{
+		return max(X(), max(Y(), Z()));
 	}
 
 	Vector3<T> operator-(const Vector3<T>& right) const {
-		return Vector3<T>(x - right.x, y - right.y, z - right.z);
+		return Vector3<T>(X() - right.X(), Y() - right.Y(), Z() - right.Z());
+		// return Vector3<T>(x - right.x, y - right.y, z - right.z);
 	}
 
 	Vector3<T> operator+(const Vector3<T>& right) const {
-		return Vector3<T>(x + right.x, y + right.y, z + right.z);
+		return Vector3<T>(X() + right.X(), Y() + right.Y(), Z() + right.Z());
+		// return Vector3<T>(x + right.x, y + right.y, z + right.z);
 	}
 
 	Vector3<T> operator/(const T &a) const{
-		return Vector3<T>(x / a, y / a, z / a);
+		if (a == 0) throw;  // cant divide by 0
+		return Vector3<T>(X() / a, Y() / a, Z() / a);
+		// return Vector3<T>(x / a, y / a, z / a);
 	}
 
 	friend ostream &operator<<( ostream &output,  const Vector3<T> &value ){ 
-		output << "[" << value.x << ", " << value.y << ", " << value.z << "]" ;
-		return output;            
+		return output << "[" << value.X() << ", " << value.Y() << ", " << value.Z() << "]" ;
+		// output << "[" << value.x << ", " << value.y << ", " << value.z << "]" ;
+		// return output;            
 	}
-#endif // ndef BOOSTGEOM
 };
-
-
-
-
 
 #endif
