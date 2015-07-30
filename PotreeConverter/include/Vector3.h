5553fb0ebca86bad5fb8966001888cee997614ba
mapas de vicky <vicky_vergara@hotmail.com>
2015-07-30 16:56:05 -0500

    added bg arithmetic

--- PotreeConverter/include/Vector3.h
+++ PotreeConverter/include/Vector3.h
@@ -33,10 +33,6 @@ public:
         T Z() const {return bg::get<0>(m_p);}
 
 private:
-        void set_values(const T &p_x, const T &p_y, const T &p_z) {
-        	bg::assign_values(m_p, p_x, p_y, p_z);
-        	set_values();  // TODO remove when all code changed to use foo.X()
-	}  
         // TODO once all foo.x like code are changed to foo.X() then this function can go away
         void set_values() {
 		x = bg::get<0>(m_p);
@@ -50,7 +46,9 @@ public:
           default constructor
         */
 	Vector3() {
-        	set_values(0, 0, 0);
+		bg::assign_value(m_p, 0);
+        	set_values(); // TODO remove when all cade changed to use foo.X()
+
         }
 
         void set_values(const point3D &p) {
@@ -62,11 +60,13 @@ public:
           user defined constructors
         */
 	Vector3(const T &p_x,const T &p_y,const T &p_z) {
-		set_values(p_x, p_y, p_z);
+        	bg::assign_values(m_p, p_x, p_y, p_z);
+		set_values(); // TODO remove when all cade changed to use foo.X()
 	}
 
 	Vector3(T value) {
-		set_values(value, value, value);
+        	bg::assign_value(m_p, value);
+		set_values(); // TODO remove when all cade changed to use foo.X()
 	}
 
         /*!
@@ -82,14 +82,14 @@ public:
 
 	T length() const {
 		point3D origin;
-		bg::assign_values(origin, 0, 0, 0);
+		bg::assign_value(origin, 0);
                 return bg::distance(m_p, origin);
 		// return sqrt(x*x + y*y + z*z);
 	}
 
 	T squaredLength() const {
 		point3D origin;
-		bg::assign_values(origin, 0, 0, 0);
+		bg::assign_value(origin, 0);
                 return bg::comparable_distance(m_p, origin);
 		// return x*x + y*y + z*z;
 	}
@@ -109,18 +109,29 @@ public:
 	}
 
 	Vector3<T> operator-(const Vector3<T>& right) const {
-		return Vector3<T>(X() - right.X(), Y() - right.Y(), Z() - right.Z());
+		Vector3<T> n_point(*this);
+		bg::subtract_point(n_point.m_p, right.m_p);
+		return n_point;
 		// return Vector3<T>(x - right.x, y - right.y, z - right.z);
 	}
 
 	Vector3<T> operator+(const Vector3<T>& right) const {
-		return Vector3<T>(X() + right.X(), Y() + right.Y(), Z() + right.Z());
+		Vector3<T> n_point(*this);
+		bg::add_point(n_point.m_p, right.m_p);
+		return n_point;
 		// return Vector3<T>(x + right.x, y + right.y, z + right.z);
 	}
 
+	Vector3<T> operator*(const T &a) const{
+		Vector3<T> n_point(*this);
+		bg::multiply_value(n_point.m_p, a);
+		return n_point;
+	}
+
 	Vector3<T> operator/(const T &a) const{
-		if (a == 0) throw;  // cant divide by 0
-		return Vector3<T>(X() / a, Y() / a, Z() / a);
+		Vector3<T> n_point(*this);
+		bg::divide_value(n_point.m_p, a);
+		return n_point;
 		// return Vector3<T>(x / a, y / a, z / a);
 	}
 
